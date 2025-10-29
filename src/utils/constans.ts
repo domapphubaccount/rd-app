f0a7c45b8f8b0204ea5be717191eacfded79a846
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-10-29 10:32:41 +0300

    fix(utils): correct typo in ROLE_ID constant and add missing semicolon
    Fix the duplicate value for "Super Admin" role and ensure proper syntax by adding missing semicolon

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -103,12 +103,14 @@ export const ROLE_ID = [
   { lable: "Admin", value: "1" },
   { lable: "User", value: "2" },
   { lable: "Super Admin", value: "2" },
-  ]
+];
+
 export const RD5_REPORT_TYPE = [
   { label: "Design", value: "Des" },
   { label: "Ins-Missing State", value: "Ms" },
   { label: "Ins-Work-interruption", value: "WI" },
 ];
+
 export const Classification = [
   { label: "SP", value: "SP" },
   { label: "NON-SP", value: "NON-SP" },
